1b7e29d649052bba2ec051c7392f7281b23a2f8b
QianGuoqing <qian_guoqing@yeah.net>
2018-07-21 22:06:21 +0800

    初始化项目

---
+++ lewen/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <mt-button type="danger">danger</mt-button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+
+</style>
